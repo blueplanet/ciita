2376e0b539d6f09bcd7fa0784a7a515e7587ef70
blueplanet <erguolinge@gmail.com>
2015-05-31 13:02:54 +0900

    Fix spec error

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe TagsController, type: :controller do
+  let!(:user) { create :user }
+  let!(:items) { create_list :item, 5 }
+  before { sign_in user }
+
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id: items.first.tags.first.name
       expect(response).to have_http_status(:success)
     end
   end
