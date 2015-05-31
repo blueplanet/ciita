a56251970f978431b5d03d54e39af379f365895f
blueplanet <erguolinge@gmail.com>
2015-05-31 09:38:09 +0900

    Item model, Items#index 追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529032538) do
+ActiveRecord::Schema.define(version: 20150531001700) do
+
+  create_table "items", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
