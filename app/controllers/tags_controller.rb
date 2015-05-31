5810c6cc6b0b900ee6ece11bbbd9588f2aceafbd
blueplanet <erguolinge@gmail.com>
2015-05-31 12:57:39 +0900

    ログインユーザだけtagsを見える

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,6 @@
 class TagsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @tag = ActsAsTaggableOn::Tag.find_by! name: params[:id]
     @items = Item.tagged_with(@tag.name).page(params[:page])
