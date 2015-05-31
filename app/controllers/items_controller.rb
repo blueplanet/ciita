e7c748000edb42d33e6315789f423d5f2eca57ce
blueplanet <erguolinge@gmail.com>
2015-05-31 19:02:02 +0900

    新規投稿機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,27 @@ class ItemsController < ApplicationController
     @items = Item.order(created_at: :desc).page(params[:page])
   end
 
+  def new
+    @item = current_user.items.build
+  end
+
   def show
     @item = Item.find params[:id]
   end
+
+  def create
+    @item = current_user.items.build item_params
+
+    if @item.save
+      redirect_to @item, notice: t('.created')
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:title, :body, :tag_list)
+    end
 end
