388f8b9ba0e4029676f2481b6e24a48730bfc810
blueplanet <erguolinge@gmail.com>
2015-06-02 19:49:31 +0900

    ユーザ編集できない不具合対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   get 'tops/show'
 
+  devise_for :users
+
   resources :items, only: %i(new create edit update index show) do
     resources :comments, only: %i(create)
   end
 
   resources :tags, only: %i(show)
   resources :users, only: %i(show)
-
-  devise_for :users
 
   root 'tops#show'
 end
