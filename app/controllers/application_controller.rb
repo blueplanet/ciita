0dd494b0456de82d3fcedb02cb606a33dece5304
blueplanet <erguolinge@gmail.com>
2015-06-04 05:28:02 +0900

    Markdownレンダリングメソッドをコントローラー側に移動する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     items_path
   end
+
+  helper_method :markdown
+
+  private
+
+    def markdown(text)
+      options = { hard_wrap: true, autolink: true, fenced_code_blocks: true }
+      Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render text
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-  def markdown(text)
-    options = { hard_wrap: true, autolink: true, fenced_code_blocks: true }
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render text
-  end
 end
