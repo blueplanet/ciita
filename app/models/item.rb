df90e0e3e1664b938bb1936014c81a29ba7faec0
blueplanet <erguolinge@gmail.com>
2015-06-04 20:09:57 +0900

    Follow設定追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ActiveRecord::Base
   has_many :comments, dependent: :nullify
 
   acts_as_taggable
+  acts_as_followable
 
   validates :title, presence: true, length: { maximum: 200 }
   validates :body, presence: true, length: { maximum: 20000, minimum: 10 }
