c580de1d8b515982228eb75020df3113ce99bd0d
blueplanet <erguolinge@gmail.com>
2015-06-07 13:59:39 +0900

    ユーザをフォローする機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i(show follow unfollow)
 
   def show
-    @user = User.find params[:id]
     @last_items = @user.items.page(params[:page])
   end
+
+  def follow
+    current_user.follow(@user)
+    redirect_to @user
+  end
+
+  def unfollow
+    current_user.stop_following(@user)
+    redirect_to @user
+  end
+
+  private
+
+    def set_user
+      @user = User.find params[:id]
+    end
 end
