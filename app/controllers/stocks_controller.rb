3d8d09d01a9b914824aa4950e4490d230cf77eab
blueplanet <erguolinge@gmail.com>
2015-06-07 10:44:02 +0900

    ストックのorderを追加

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,6 +2,6 @@ class StocksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @stocks = current_user.following_items.page(params[:page])
+    @stocks = current_user.following_items.order(updated_at: :desc).page(params[:page])
   end
 end
