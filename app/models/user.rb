3f201f0ea00be3ba6bb616d6393dc069f614259e
blueplanet <erguolinge@gmail.com>
2015-06-07 11:10:08 +0900

    gravatarの画像を表示する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  include Gravtastic
+  gravtastic
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
