5ac4d92e47252d6ee3b5099f9a2967944fc31949
blueplanet <erguolinge@gmail.com>
2015-06-11 06:02:08 +0900

    Add uniq

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,9 @@ class Item < ActiveRecord::Base
 
   scope :feed_items_for, -> (user) do
     joins(:taggings, :user).
-    where 'taggings.tag_id IN (:tag_ids) OR users.id IN (:user_ids)',
+    where('taggings.tag_id IN (:tag_ids) OR users.id IN (:user_ids)',
       tag_ids: user.following_by_type(ActsAsTaggableOn::Tag.to_s).pluck(:id),
       user_ids: user.following_users.pluck(:id)
+    ).uniq
   end
 end
